fb3154fd55802047d705bc8cbd89ec9aff73ceac
Lucas <85760849+LuKarbo@users.noreply.github.com>
2024-09-09 15:36:41 -0300

    feat: Banners de 3 imgs (ViewPager2 + Adapter) [Buscar más info]

--- app/src/main/java/ar/edu/davinci/carbone_lucas/lk_store/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/carbone_lucas/lk_store/MainActivity.java
@@ -2,19 +2,52 @@ package ar.edu.davinci.carbone_lucas.lk_store;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private Handler handler;
+    private Runnable runnable;
+    private List<Integer> bannerImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        // ESTUDIAR MEJOR SOBRE ViewPager y IMGs
+        // ------------------------------------------
+        viewPager = findViewById(R.id.viewPager);
+
+        bannerImages = Arrays.asList(R.drawable.hambur_1, R.drawable.hambur_2, R.drawable.hambur_3);
+        BannerAdapter adapter = new BannerAdapter(this, bannerImages);
+        viewPager.setAdapter(adapter);
+        handler = new Handler(Looper.getMainLooper());
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                int nextItem = (viewPager.getCurrentItem() + 1) % bannerImages.size();
+                viewPager.setCurrentItem(nextItem);
+                handler.postDelayed(this, 3000);
+            }
+        };
+        handler.postDelayed(runnable, 8000);
+        // ------------------------------------------
     }
 }
