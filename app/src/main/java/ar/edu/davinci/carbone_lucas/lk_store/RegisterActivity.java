31e6b06e5592b29b21e933dd32f43535021599f7
Lucas <85760849+LuKarbo@users.noreply.github.com>
2024-09-12 14:59:45 -0300

    Fix: Errores de BTNs y gotos
    Solucionado:
    - BTN Register no funcionaba
    - Error de tipeos de gotos

--- app/src/main/java/ar/edu/davinci/carbone_lucas/lk_store/RegisterActivity.java
+++ app/src/main/java/ar/edu/davinci/carbone_lucas/lk_store/RegisterActivity.java
@@ -3,6 +3,7 @@ package ar.edu.davinci.carbone_lucas.lk_store;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +12,13 @@ public class RegisterActivity extends AppCompatActivity {
 
     TextView gotoLogin;
 
+    Button regiserBTN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         // login_goto
         gotoLogin = findViewById(R.id.register_goto);
 
@@ -26,5 +30,18 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intentLogin);
             }
         });
+
+        // regiser btn
+        regiserBTN = findViewById(R.id.registerBTN);
+
+        regiserBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intentLogin);
+            }
+        });
+
+
     }
 }
