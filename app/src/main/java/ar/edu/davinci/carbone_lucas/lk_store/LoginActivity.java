e9c957ab96b6ad4bebfb5d4d91516174d8f0e5fb
Lucas <85760849+LuKarbo@users.noreply.github.com>
2024-09-26 13:22:44 -0300

    feat: Mofigicacar Lógica de Login
    - Se saco el XML del Button y Layour de mensajes
    - Se edito el LoginActivity
    - Se agrego la función createAlertTextView()
    - Se creo la función createLoginButton()
    - Se creo la función createAccountMessage()
    
    Se realizaron los cambios para mantener más limpio el OnCreate y generar 3 "objetos" desde java y no en XML

--- app/src/main/java/ar/edu/davinci/carbone_lucas/lk_store/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/carbone_lucas/lk_store/LoginActivity.java
@@ -1,40 +1,66 @@
 package ar.edu.davinci.carbone_lucas.lk_store;
 
 import android.content.Intent;
-import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView gotoRegister;
-    Button gotoHome;
-    TextView alert;
+    private Button loginButton;
+    private TextView alert;
+    private EditText nameInput;
+    private EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView name = findViewById(R.id.nameInput);
-        TextView pass = findViewById(R.id.passwordInput);
+        nameInput = findViewById(R.id.nameInput);
+        passwordInput = findViewById(R.id.passwordInput);
 
+        createAlertTextView();
+        createLoginButton();
+        createAccountMessage();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                if (name.equals("test") && password.equals("123")) {
+                    alert.setText("Credenciales inválidas");
+                    alert.setVisibility(View.VISIBLE);
+                } else {
+                    Intent intentHome = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intentHome);
+                }
+            }
+        });
+    }
+
+    // creo el mensaje de Alerta del onCreate para dejarlo más limpio
+    private void createAlertTextView() {
         alert = new TextView(this);
         alert.setId(View.generateViewId());
         alert.setTextColor(getResources().getColor(R.color.alert));
         alert.setTextSize(12);
         alert.setGravity(View.TEXT_ALIGNMENT_CENTER);
         alert.setVisibility(View.GONE);
-        LayoutParams layoutParams = new LayoutParams(
-                LayoutParams.WRAP_CONTENT,
-                LayoutParams.WRAP_CONTENT
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
         );
         LinearLayout layout = findViewById(R.id.dataContainer_login);
         layoutParams.startToStart = layout.getId();
@@ -42,40 +68,81 @@ public class LoginActivity extends AppCompatActivity {
 
         alert.setLayoutParams(layoutParams);
         layout.addView(alert, 0);
+    }
 
-        gotoHome = findViewById(R.id.loginBTN);
+    // creo el Boton de Login fuera del onCreate para dejarlo más limpio
+    private void createLoginButton() {
+        GradientDrawable shape = new GradientDrawable();
+        shape.setShape(GradientDrawable.RECTANGLE);
+        shape.setColor(ContextCompat.getColor(this, R.color.gotoRegisterLogin));
+        shape.setCornerRadius(dpToPx(30));
 
-        gotoHome.setOnClickListener(new View.OnClickListener() {
+        loginButton = new Button(this);
+        loginButton.setId(View.generateViewId());
+        loginButton.setText(R.string.ingresar);
+        loginButton.setTextColor(ContextCompat.getColor(this, R.color.white));
+        loginButton.setTextSize(17);
+        loginButton.setBackground(shape);
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                dpToPx(320),
+                dpToPx(60)
+        );
+        layoutParams.topToBottom = R.id.passwordInput;
+        layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topMargin = dpToPx(16);
+
+        LinearLayout layout = findViewById(R.id.dataContainer_login);
+        loginButton.setLayoutParams(layoutParams);
+        layout.addView(loginButton);
+    }
+
+    // creo el mensaje fuera del onCreate para dejarlo más limpio
+    private void createAccountMessage() {
+        LinearLayout containerMsg = new LinearLayout(this);
+        containerMsg.setId(View.generateViewId());
+        containerMsg.setOrientation(LinearLayout.HORIZONTAL);
+        containerMsg.setGravity(android.view.Gravity.CENTER);
+
+        LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        containerParams.topMargin = dpToPx(16);
+        containerMsg.setLayoutParams(containerParams);
+
+        TextView haveAccountMsg = new TextView(this);
+        haveAccountMsg.setText(R.string.notHaveAccount);
+        haveAccountMsg.setTextColor(ContextCompat.getColor(this, R.color.white));
+
+        TextView registerGoto = new TextView(this);
+        registerGoto.setText(R.string.notHaveAccount_goto);
+        registerGoto.setTextColor(ContextCompat.getColor(this, R.color.gotoRegisterLogin));
+        LinearLayout.LayoutParams registerGotoParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        registerGotoParams.leftMargin = dpToPx(5);
+        registerGoto.setLayoutParams(registerGotoParams);
+
+        containerMsg.addView(haveAccountMsg);
+        containerMsg.addView(registerGoto);
+
+        LinearLayout dataContainer = findViewById(R.id.dataContainer_login);
+        dataContainer.addView(containerMsg);
+
+        registerGoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // validaciones para ver si es un usuario cargado
-                // etc
-                // etc
-
-                if(name.getText().toString().equals("test") && pass.getText().toString().equals("123"))
-                {
-                    // aviso de fallo
-                    alert.setText("Credenciales inválidas");
-                    alert.setVisibility(View.VISIBLE);
-                }
-                else{
-                    // cambio a la vista del register
-                    Intent intentHome = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intentHome);
-                }
-            }
-        });
-
-        gotoRegister = findViewById(R.id.register_goto);
-
-        gotoRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // cambio a la vista del register
                 Intent intentRegister = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intentRegister);
             }
         });
+    }
 
+    private int dpToPx(int dp) {
+        float density = getResources().getDisplayMetrics().density;
+        return Math.round((float) dp * density);
     }
 }
